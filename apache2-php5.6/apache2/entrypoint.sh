5c822ffffb60bf5fa2c0943602ffb55d12a08c89
Viniciusalopes <suporte@viniciusalopes.com.br>
2021-11-03 17:27:01 -0300

    Storage files

--- apache2-php5.6/apache2/entrypoint.sh
+++ apache2-php5.6/apache2/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Permanent files
+DIR_VOVO="/var/www/vovo"
+DIR_HTML="/var/www/html"
+DIR_APACHE="/etc/apache2"
+ln -sfv ${DIR_VOVO}/html/* ${DIR_HTML}/
+ln -sfv ${DIR_VOVO}/conf/* ${DIR_APACHE}/
+ln -sfv ${DIR_VOVO}/sites-available/* ${DIR_APACHE}/sites-available/
+
 source /etc/apache2/envvars
 #tail -F /var/log/apache2/* &
-exec apache2 -D FOREGROUND
+exec apache2 -D FOREGROUND 
