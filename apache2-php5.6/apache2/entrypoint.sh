e14748eba56578192b41d6f5121f769d8f906a80
vinicius.lopes <vinicius.lopes@goias.gov.br>
2021-11-13 20:01:29 -0300

    Copy files /var/www/html

--- apache2-php5.6/apache2/entrypoint.sh
+++ apache2-php5.6/apache2/entrypoint.sh
@@ -4,9 +4,16 @@
 DIR_VOVO="/var/www/vovo"
 DIR_HTML="/var/www/html"
 DIR_APACHE="/etc/apache2"
-ln -sfv ${DIR_VOVO}/html/* ${DIR_HTML}/
-ln -sfv ${DIR_VOVO}/conf/* ${DIR_APACHE}/
-ln -sfv ${DIR_VOVO}/sites-available/* ${DIR_APACHE}/sites-available/
+
+mkdir -pv ${DIR_HTML}/css
+mkdir -pv ${DIR_HTML}/img
+mkdir -pv ${DIR_APACHE}/sites-available/
+
+cp -v ${DIR_VOVO}/html/* ${DIR_HTML}/
+cp -v ${DIR_VOVO}/html/css/* ${DIR_HTML}/css/
+cp -v ${DIR_VOVO}/html/img/* ${DIR_HTML}/img/
+cp -v ${DIR_VOVO}/conf/* ${DIR_APACHE}/
+cp -v ${DIR_VOVO}/sites-available/* ${DIR_APACHE}/sites-available/
 
 source /etc/apache2/envvars
 #tail -F /var/log/apache2/* &
